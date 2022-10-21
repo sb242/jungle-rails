426b78fcb0557f8f4a8670ff30fdf74f65b372b4
Spencer <spencerb242@proton.me>
2022-10-20 22:31:34 -0600

    fixed about page hyperlink

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,9 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
-
+  
+  get "/about", to: "about#show"
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
